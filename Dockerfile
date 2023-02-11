d7506a12e8918aafe136778ce352f451c9c52046
walkthunder <walkthunder@163.com>
2023-02-11 12:37:04 +0800

    feat(server): Update CI server config.

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,36 @@
-FROM alpine:latest
+# 二开推荐阅读[如何提高项目构建效率](https://developers.weixin.qq.com/miniprogram/dev/wxcloudrun/src/scene/build/speed.html)
+FROM node:lts-alpine
 
-RUN apk add nodejs
-RUN apk add npm
+# 容器默认时区为UTC，如需使用上海时间请启用以下时区设置命令
+# RUN apk add tzdata && cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo Asia/Shanghai > /etc/timezone
 
-COPY ./ /var/chatgpt-api
+# 使用 HTTPS 协议访问容器云调用证书安装
+# RUN apk add ca-certificates
 
+# 安装依赖包，如需其他依赖包，请到alpine依赖包管理(https://pkgs.alpinelinux.org/packages?name=php8*imagick*&branch=v3.13)查找。
+# 选用国内镜像源以提高下载速度
+# RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.tencent.com/g' /etc/apk/repositories \
+# && apk add --update --no-cache nodejs npm
+
+# # 指定工作目录
 WORKDIR /var/chatgpt-api
-RUN npm ci --no-color --quiet
 
-ENV API_HOST=0.0.0.0
+# 拷贝包管理文件
+COPY package*.json /var/chatgpt-api/
 
-EXPOSE 3000
+# npm 源，选用国内镜像源以提高下载速度
+RUN npm config set registry https://mirrors.cloud.tencent.com/npm/
+# RUN npm config set registry https://registry.npm.taobao.org/
 
-ENTRYPOINT npm start
+# npm 安装依赖
+RUN npm install
+
+# 将当前目录（dockerfile所在目录）下所有文件都拷贝到工作目录下（.dockerignore中文件除外）
+COPY . /var/chatgpt-api
+
+EXPOSE 8160
+
+# 执行启动命令
+# 写多行独立的CMD命令是错误写法！只有最后一行CMD命令会被执行，之前的都会被忽略，导致业务报错。
+# 请参考[Docker官方文档之CMD命令](https://docs.docker.com/engine/reference/builder/#cmd)
+CMD ["npm", "start"]
--- bin/server.js
+++ bin/server.js
@@ -2,24 +2,28 @@
 import fastify from 'fastify';
 import cors from '@fastify/cors';
 import { FastifySSEPlugin } from "fastify-sse-v2";
+import fastifyStatic from '@fastify/static';
 import fs from 'fs';
+import path from 'path';
+import CryptoJS from "crypto-js";
 import { pathToFileURL } from 'url'
+
 import ChatGPTClient from '../src/ChatGPTClient.js';
 import BingAIClient from '../src/BingAIClient.js';
 import { KeyvFile } from 'keyv-file';
 
 const arg = process.argv.find((arg) => arg.startsWith('--settings'));
-let path;
+let settingPath;
 if (arg) {
-    path = arg.split('=')[1];
+    settingPath = arg.split('=')[1];
 } else {
-    path = './settings.js';
+    settingPath = './settings.js';
 }
 
 let settings;
-if (fs.existsSync(path)) {
-    // get the full path
-    const fullPath = fs.realpathSync(path);
+if (fs.existsSync(settingPath)) {
+    // get the full settingPath
+    const fullPath = fs.realpathSync(settingPath);
     settings = (await import(pathToFileURL(fullPath).toString())).default;
 } else {
     if (arg) {
@@ -65,11 +69,45 @@ switch (clientToUse) {
 const server = fastify();
 
 await server.register(FastifySSEPlugin);
+
+server.register(fastifyStatic, {
+    root: fs.realpathSync('.'),
+    prefix: '/'
+})
 await server.register(cors, {
     origin: '*',
 });
 
-server.post('/conversation', async (request, reply) => {
+server.get('/', async (req, res) => {
+    res.code(200);
+    res.send('ok')
+})
+server.get('/MP_verify_ucmvXzViscnLif9o.txt', async (req, reply) => {
+    return reply.sendFile("MP_verify_ucmvXzViscnLif9o.txt");
+})
+  
+
+server.post('/api/chat', async (request, reply) => {
+    console.log('api chat message - ', JSON.stringify(request.body));
+    try {
+        const { hash } = request.body || {}
+        if (!hash) {
+        throw new Error('Not Authorized')
+        }
+        console.log('hash and salt: ', hash, process.env.CHAT_SALT);
+        const bytes  = CryptoJS.AES.decrypt(hash, process.env.CHAT_SALT);
+        console.log('request decrypt: ', bytes);
+        const { id, openId, left, date } = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+        console.log('request hash data: ', id, openId, left, date);
+        if (Math.abs(new Date().valueOf() - Number(date)) > 20000) {
+            throw new Error('Verify Failed')
+        }
+        // Continue biz
+        // TODO: Throttle by openId
+        // return chatGptHandler(req, res);
+    } catch (error) {
+        res.status(400).send(error?.message || 'Auth Failed')
+    }
     const body = request.body || {};
 
     let onProgress;
