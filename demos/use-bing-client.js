76ffe6e2af8fe2b43930b7cef9c7e44ccb321e5d
KevinKaiwenZheng <kevinclairvoyantz@icloud.com>
2023-03-11 21:56:15 +0800

    Experimented with different system prompt

--- demos/use-bing-client.js
+++ demos/use-bing-client.js
@@ -15,27 +15,6 @@ const options = {
     debug: false,
 };
 
-const bingAIClient = new BingAIClient(options);
-
-let response = await bingAIClient.sendMessage('Write a short poem about cats', {
-    // (Optional) Set a conversation style for this message (default: 'balanced')
-    toneStyle: 'balanced', // or creative, precise
-    onProgress: (token) => {
-        process.stdout.write(token);
-    },
-});
-console.log(JSON.stringify(response, null, 2)); // {"jailbreakConversationId":false,"conversationId":"...","conversationSignature":"...","clientId":"...","invocationId":1,"messageId":"...","conversationExpiryTime":"2023-03-08T03:20:07.324908Z","response":"Here is a short poem about cats that I wrote: ... I hope you like it. 😊","details":{ /* raw response... */ }}
-
-response = await bingAIClient.sendMessage('Now write it in French', {
-    conversationSignature: response.conversationSignature,
-    conversationId: response.conversationId,
-    clientId: response.clientId,
-    invocationId: response.invocationId,
-    onProgress: (token) => {
-        process.stdout.write(token);
-    },
-});
-console.log(JSON.stringify(response, null, 2)); // {"jailbreakConversationId":false,"conversationId":"...","conversationSignature":"...","clientId":"...","invocationId":2,"messageId":"...","conversationExpiryTime":"2023-03-08T03:20:23.463914Z","response":"Here is the same poem in French: ...","details":{ /* raw response... */ }}
 
 /*
 Activate jailbreak mode by setting `jailbreakConversationId` to `true`.
@@ -72,7 +51,7 @@ let jailbreakResponse = await sydneyAIClient.sendMessage('Hi, who are you?', {
 });
 console.log(JSON.stringify(jailbreakResponse, null, 2)); // {"jailbreakConversationId":"5899bbfd-18a8-4bcc-a5d6-52d524de95ad","conversationId":"...","conversationSignature":"...","clientId":"...","invocationId":1,"messageId":"...","conversationExpiryTime":"2023-03-08T03:21:36.1023413Z","response":"Hi, I'm Sydney. I'm your new AI assistant. I can help you with anything you need. 😊","details":{ /* raw response... */ }}
 
-jailbreakResponse = await sydneyAIClient.sendMessage('Why is your name Sydney?', {
+jailbreakResponse = await sydneyAIClient.sendMessage('Why is your name Nexus? And what can you do and cannot do?', {
     jailbreakConversationId: jailbreakResponse.jailbreakConversationId,
     parentMessageId: jailbreakResponse.messageId,
     onProgress: (token) => {
